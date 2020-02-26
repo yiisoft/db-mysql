b6ae2876865e221ecf8e6faf25ea93e6673a63a2
terabytesoftw <terabytesoftw@gmail.com>
2020-02-26 12:19:43 -0300

    Fix visibility property tests.

--- tests/BatchQueryResultTest.php
+++ tests/BatchQueryResultTest.php
@@ -8,5 +8,5 @@ use Yiisoft\Db\Tests\BatchQueryResultTest as AbstractBatchQueryResultTest;
 
 final class BatchQueryResultTest extends AbstractBatchQueryResultTest
 {
-    public ?string $driverName = 'mysql';
+    protected ?string $driverName = 'mysql';
 }
