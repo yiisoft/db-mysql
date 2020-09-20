ce6456bf7779b41502ccc5deb503830396f2583a
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2020-09-20 10:17:01 -0300

    Add SqlDataProviderTest. (#49)

---
+++ tests/SqlDataProviderTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Db\Mysql\Tests;
+
+use Yiisoft\Db\TestUtility\TestSqlDataProviderTrait;
+
+/**
+ * @group mysql
+ */
+final class SqlDataProviderTest extends TestCase
+{
+    use TestSqlDataProviderTrait;
+}
