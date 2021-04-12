39d282bbf82af3fdaf45b7d7c106fda20d0dd469
Alexander Makarov <sam@rmcreative.ru>
2021-04-13 01:42:10 +0300

    Adjust namespaces

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -24,7 +24,7 @@ use Yiisoft\Db\Factory\DatabaseFactory;
 use Yiisoft\Db\Mysql\Connection;
 use Yiisoft\Db\TestUtility\IsOneOfAssert;
 use Yiisoft\Di\Container;
-use Yiisoft\Factory\Definitions\Reference;
+use Yiisoft\Factory\Definition\Reference;
 use Yiisoft\Log\Logger;
 use Yiisoft\Profiler\Profiler;
 use Yiisoft\Profiler\ProfilerInterface;
