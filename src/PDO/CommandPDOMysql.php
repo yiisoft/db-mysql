df8f9e9c83ed083ee1d0561489811c1ad48c0218
darkdef <darkdef@gmail.com>
2022-03-26 21:12:08 +0300

    CommandPdo -> CommandPDO

--- src/PDO/CommandPDOMysql.php
+++ src/PDO/CommandPDOMysql.php
@@ -6,14 +6,14 @@ namespace Yiisoft\Db\Mysql\PDO;
 
 use PDOException;
 use Yiisoft\Db\Cache\QueryCache;
-use Yiisoft\Db\Command\CommandPdo;
+use Yiisoft\Db\Command\CommandPDO;
 use Yiisoft\Db\Connection\ConnectionPDOInterface;
 use Yiisoft\Db\Exception\ConvertException;
 use Yiisoft\Db\Exception\Exception;
 use Yiisoft\Db\Exception\InvalidConfigException;
 use Yiisoft\Db\Query\QueryBuilderInterface;
 
-final class CommandPDOMysql extends CommandPdo
+final class CommandPDOMysql extends CommandPDO
 {
     public function __construct(private ConnectionPDOInterface $db, QueryCache $queryCache)
     {
