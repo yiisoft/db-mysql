f9d2dd4fead127673f5b70d9d123539168075997
Valerii Gorbachev <darkdef@gmail.com>
2022-04-15 23:09:10 +0300

    Common constructor for ConnectionPDO (#106)

--- src/PDO/ConnectionPDOMysql.php
+++ src/PDO/ConnectionPDOMysql.php
@@ -5,11 +5,8 @@ declare(strict_types=1);
 namespace Yiisoft\Db\Mysql\PDO;
 
 use PDO;
-use Yiisoft\Db\Cache\QueryCache;
-use Yiisoft\Db\Cache\SchemaCache;
 use Yiisoft\Db\Command\CommandInterface;
 use Yiisoft\Db\Connection\ConnectionPDO;
-use Yiisoft\Db\Driver\PDODriver;
 use Yiisoft\Db\Exception\Exception;
 use Yiisoft\Db\Exception\InvalidConfigException;
 use Yiisoft\Db\Mysql\Quoter;
@@ -26,14 +23,6 @@ use function constant;
  */
 final class ConnectionPDOMysql extends ConnectionPDO
 {
-    public function __construct(
-        protected PDODriver $driver,
-        protected QueryCache $queryCache,
-        protected SchemaCache $schemaCache
-    ) {
-        parent::__construct($queryCache);
-    }
-
     public function createCommand(?string $sql = null, array $params = []): CommandInterface
     {
         $command = new CommandPDOMysql($this, $this->queryCache);
