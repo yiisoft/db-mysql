08cf07523c711bee084554e4a48182a60abfe13e
darkdef <darkdef@gmail.com>
2022-05-10 12:24:03 +0300

    fix for php81

--- tests/QueryTest.php
+++ tests/QueryTest.php
@@ -39,9 +39,9 @@ final class QueryTest extends TestCase
 
         $result = $query->column();
 
-        /** @todo need fix for this behaviour PHP8.1 + pdo_mysql */
-        $this->assertTrue(in_array('2', $result, false));
-        $this->assertTrue(in_array('3', $result, false));
-        $this->assertFalse(in_array('1', $result, false));
+        $this->assertCount(2, $result);
+        $this->assertContains('2', $result);
+        $this->assertContains('3', $result);
+        $this->assertNotContains('1', $result);
     }
 }
