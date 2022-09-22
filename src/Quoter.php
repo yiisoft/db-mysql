39ad8bae9f33472533da34d3362f96e24bd3a6a1
darkdef <darkdef@gmail.com>
2022-09-22 21:39:35 +0300

    fix

--- src/Quoter.php
+++ src/Quoter.php
@@ -4,23 +4,10 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\Mysql;
 
-use PDO;
 use Yiisoft\Db\Schema\Quoter as BaseQuoter;
 
 final class Quoter extends BaseQuoter
 {
-    /**
-     * @psalm-param string[]|string $columnQuoteCharacter
-     * @psalm-param string[]|string $tableQuoteCharacter
-     */
-    public function __construct(
-        array|string $columnQuoteCharacter,
-        array|string $tableQuoteCharacter,
-        string $tablePrefix = ''
-    ) {
-        parent::__construct($columnQuoteCharacter, $tableQuoteCharacter, $tablePrefix);
-    }
-
     public function quoteValue(mixed $value): mixed
     {
         if (!is_string($value)) {
