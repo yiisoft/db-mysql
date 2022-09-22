829923b84cb571c35dcba7f054f8bc5180b3d4a6
darkdef <darkdef@gmail.com>
2022-09-22 21:47:34 +0300

    fix

--- src/ConnectionPDO.php
+++ src/ConnectionPDO.php
@@ -82,7 +82,7 @@ final class ConnectionPDO extends AbstractConnectionPDO
     public function getQuoter(): QuoterInterface
     {
         if ($this->quoter === null) {
-            $this->quoter = new Quoter( '`', '`', $this->getTablePrefix());
+            $this->quoter = new Quoter('`', '`', $this->getTablePrefix());
         }
 
         return $this->quoter;
