95744072892833f7e4f224fe94bd9f6662031145
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2022-12-30 14:11:33 -0300

    Improvements Dsn::class, fix phpdoc, add more tests. (#193)
    * Improvements Dsn::class, fix phpdoc, add more tests.

---
+++ tests/DsnTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Db\Mysql\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Db\Mysql\Dsn;
+
+/**
+ * @group mysql
+ *
+ * @psalm-suppress PropertyNotSetInConstructor
+ */
+final class DsnTest extends TestCase
+{
+    public function testAsString(): void
+    {
+        $this->assertSame(
+            'mysql:host=localhost;dbname=yiitest;port=3306',
+            (new Dsn('mysql', 'localhost', 'yiitest'))->asString(),
+        );
+    }
+
+    public function testAsStringWithOptions(): void
+    {
+        $this->assertSame(
+            'mysql:host=localhost;dbname=yiitest;port=3307;charset=utf8',
+            (new Dsn('mysql', 'localhost', 'yiitest', '3307', ['charset' => 'utf8']))->asString(),
+        );
+    }
+
+    public function testAsStringWithPort(): void
+    {
+        $this->assertSame(
+            'mysql:host=localhost;dbname=yiitest;port=3307',
+            (new Dsn('mysql', 'localhost', 'yiitest', '3307'))->asString(),
+        );
+    }
+}
