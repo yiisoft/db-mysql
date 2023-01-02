28f4519752fc2821eb95a4d809e90a50e6b7dd24
Wilmer Arambula <terabytesoftw@gmail.com>
2023-01-02 09:36:16 -0300

    add `testAmbiguousColumnIndexBy()`.

--- tests/QueryTest.php
+++ tests/QueryTest.php
@@ -22,6 +22,27 @@ final class QueryTest extends CommonQueryTest
     use TestTrait;
 
     /**
+     * Ensure no ambiguous column error occurs on indexBy with JOIN.
+     *
+     * @link https://github.com/yiisoft/yii2/issues/13859
+     */
+    public function testAmbiguousColumnIndexBy(): void
+    {
+        $db = $this->getConnection(true);
+
+        $selectExpression = "concat(customer.name,' in ', p.description) name";
+
+        $result = (new Query($db))
+            ->select([$selectExpression])
+            ->from('customer')
+            ->innerJoin('profile p', '[[customer]].[[profile_id]] = [[p]].[[id]]')
+            ->indexBy('id')
+            ->column();
+
+        $this->assertSame([1 => 'user1 in profile customer 1', 3 => 'user3 in profile customer 3'], $result);
+    }
+
+    /**
      * @throws Exception
      * @throws InvalidConfigException
      * @throws Throwable
