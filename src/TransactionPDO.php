012e67723b2d1b0a73b836bfc0de28aa5e9ce02d
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-01-05 11:18:55 -0300

    Use prefix Abstract in abstract class command. (#198)
    * Use prefix Abstract in abstract class command.

--- src/TransactionPDO.php
+++ src/TransactionPDO.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\Mysql;
 
-use Yiisoft\Db\Driver\PDO\TransactionPDO as AbstractTransactionPDO;
+use Yiisoft\Db\Driver\PDO\AbstractTransactionPDO;
 
 final class TransactionPDO extends AbstractTransactionPDO
 {
