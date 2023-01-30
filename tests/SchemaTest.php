86e468b13ca2234ccc8aa15178e63c5dad61468d
darkdef <darkdef@gmail.com>
2023-01-30 22:25:40 +0300

    styleci

--- tests/SchemaTest.php
+++ tests/SchemaTest.php
@@ -486,7 +486,8 @@ final class SchemaTest extends CommonSchemaTest
         }
 
         $db->createCommand()->createTable(
-            $tableName, [
+            $tableName,
+            [
                 'id' =>  $db->getSchema()->createColumnSchemaBuilder(Schema::TYPE_PK),
                 'bool_col' =>  $db->getSchema()->createColumnSchemaBuilder(Schema::TYPE_BOOLEAN),
                 'status' =>  $db->getSchema()->createColumnSchemaBuilder(Schema::TYPE_TINYINT, 1),
