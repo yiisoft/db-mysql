d26335110537648c44927ecd0acbc2d6ab4729e5
Wilmer Arambula <terabytesoftw@gmail.com>
2023-03-01 16:19:03 -0300

    Implement `DsnSocket::class` for dsn with unix sockets.

---
+++ src/DsnSocket.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Db\Mysql;
+
+use Yiisoft\Db\Connection\AbstractDsnSocket;
+
+/**
+ * The Dsn class is typically used to parse a DSN string, which is a string that contains all the necessary information
+ * to connect to a database, such as the database driver, unix socket, database name, options.
+ *
+ * It also allows you to access individual components of the DSN, such as the driver or the database name.
+ */
+final class DsnSocket extends AbstractDsnSocket
+{
+}
