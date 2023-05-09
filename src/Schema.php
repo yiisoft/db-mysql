ae3a13586dcb84b14f806056f24116d6c95c4fa9
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-05-09 13:23:47 +0300

    Apply suggestions

--- src/Schema.php
+++ src/Schema.php
@@ -906,8 +906,6 @@ final class Schema extends AbstractPdoSchema
      * This method may be overridden by child classes to create a DBMS-specific column schema.
      *
      * @param string $name Name of the column.
-     *
-     * @return ColumnSchema
      */
     private function createColumnSchema(string $name): ColumnSchema
     {
