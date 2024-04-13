bf11e0f694e171dad7ef666966089122a3ae1bb2
Sergei Tigrov <rrr-r@ya.ru>
2024-04-13 15:03:07 +0700

    Allow scalar values for `Query::select()` (#329)
    * Update tests according main PR
    
    * Add type string to test
    
    ---------
    
    Co-authored-by: Sergei Predvoditelev <sergei@predvoditelev.ru>

--- tests/QueryBuilderTest.php
+++ tests/QueryBuilderTest.php
@@ -653,4 +653,10 @@ final class QueryBuilderTest extends CommonQueryBuilderTest
     ): void {
         parent::testUpsertExecute($table, $insertColumns, $updateColumns);
     }
+
+    /** @dataProvider \Yiisoft\Db\Mysql\Tests\Provider\QueryBuilderProvider::selectScalar */
+    public function testSelectScalar(array|bool|float|int|string $columns, string $expected): void
+    {
+        parent::testSelectScalar($columns, $expected);
+    }
 }
