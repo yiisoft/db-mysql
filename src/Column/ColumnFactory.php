ec0fbc8528337a53048c17888925293ce4eb33d3
Tigrov <rrr-r@ya.ru>
2024-11-08 19:49:36 +0700

    Refactor `normalizeDefaultValue()`

--- src/Column/ColumnFactory.php
+++ src/Column/ColumnFactory.php
@@ -5,7 +5,15 @@ declare(strict_types=1);
 namespace Yiisoft\Db\Mysql\Column;
 
 use Yiisoft\Db\Constant\ColumnType;
+use Yiisoft\Db\Expression\Expression;
 use Yiisoft\Db\Schema\Column\AbstractColumnFactory;
+use Yiisoft\Db\Schema\Column\ColumnSchemaInterface;
+
+use function bindec;
+use function in_array;
+use function preg_match;
+use function substr;
+use function trim;
 
 final class ColumnFactory extends AbstractColumnFactory
 {
@@ -54,5 +62,42 @@ final class ColumnFactory extends AbstractColumnFactory
         }
 
         return parent::getType($dbType, $info);
+    }
+
+    protected function normalizeDefaultValue(string|null $defaultValue, ColumnSchemaInterface $column): mixed
+    {
+        if (
+            $defaultValue === null
+            || $column->isPrimaryKey()
+            || $column->isComputed()
+        ) {
+            return null;
+        }
+
+        return $this->normalizeNotNullDefaultValue($defaultValue, $column);
+    }
+
+    protected function normalizeNotNullDefaultValue(string $defaultValue, ColumnSchemaInterface $column): mixed
+    {
+        if (
+            in_array($column->getType(), [ColumnType::TIMESTAMP, ColumnType::DATETIME, ColumnType::DATE, ColumnType::TIME], true)
+            && preg_match('/^current_timestamp(?:\((\d*)\))?$/i', $defaultValue, $matches) === 1
+        ) {
+            return new Expression('CURRENT_TIMESTAMP' . (!empty($matches[1]) ? '(' . $matches[1] . ')' : ''));
+        }
+
+        if (!empty($defaultValue) && !empty($column->getExtra())) {
+            return new Expression($defaultValue);
+        }
+
+        if ($column->getDbType() === 'bit') {
+            return $column->phpTypecast(bindec(trim($defaultValue, "b'")));
+        }
+
+        if ($defaultValue[0] === "'" && $defaultValue[-1] === "'") {
+            return $column->phpTypecast(substr($defaultValue, 1, -1));
+        }
+
+        return $column->phpTypecast($defaultValue);
     }
 }
