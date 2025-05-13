de66354622749503728fb413265c5588eade5dac
Tigrov <rrr-r@ya.ru>
2025-05-13 17:11:02 +0700

    Add line to CHANGELOG.md and docs [skip ci]

--- src/Column/DateTimeColumn.php
+++ src/Column/DateTimeColumn.php
@@ -6,6 +6,21 @@ namespace Yiisoft\Db\Mysql\Column;
 
 use Yiisoft\Db\Constant\ColumnType;
 
+/**
+ * Represents the metadata for a datetime column.
+ *
+ * > [!WARNING]
+ * > MySQL DBMS converts `TIMESTAMP` column type values from database session time zone to UTC for storage, and back
+ * > from UTC to the session time zone when retrieve the values.
+ *
+ * `TIMESTAMP` database type does not store time zone offset and require to convert datetime values to the database
+ * session time zone before insert and back to the PHP time zone after retrieve the values. This will be done in the
+ * {@see dbTypecast()} and {@see phpTypecast()} methods and guarantees that the values are stored in the database
+ * in the correct time zone.
+ *
+ * To avoid possible time zone issues with the datetime values conversion, it is recommended to set the PHP and database
+ * time zones to UTC.
+ */
 final class DateTimeColumn extends \Yiisoft\Db\Schema\Column\DateTimeColumn
 {
     protected function getFormat(): string
