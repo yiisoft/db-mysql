87e5ea7d63b092cf2475b260fcdadb2f622eb948
Sergei Tigrov <rrr-r@ya.ru>
2025-05-17 14:52:09 +0700

    Update src/DMLQueryBuilder.php [skip ci]
    Co-authored-by: Sergei Predvoditelev <sergei@predvoditelev.ru>

--- src/DMLQueryBuilder.php
+++ src/DMLQueryBuilder.php
@@ -21,7 +21,7 @@ final class DMLQueryBuilder extends AbstractDMLQueryBuilder
     /** @throws NotSupportedException */
     public function insertWithReturningPks(string $table, array|QueryInterface $columns, array &$params = []): string
     {
-        throw new NotSupportedException(__METHOD__ . ' is not supported by Mysql.');
+        throw new NotSupportedException(__METHOD__ . ' is not supported by MySql.');
     }
 
     public function resetSequence(string $table, int|string|null $value = null): string
