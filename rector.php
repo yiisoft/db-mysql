f21fe697200f0c7aa6d639231eb7c037f7fe1d75
Tigrov <rrr-r@ya.ru>
2025-06-26 21:54:49 +0700

    Refactor workflows

--- rector.php
+++ rector.php
@@ -7,30 +7,18 @@ use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\StaticCall\RemoveParentCallWithoutParentRector;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
 use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
-use Rector\Set\ValueObject\LevelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/src',
-        /**
-         * Disabled ./tests directory due to PR #307
-         *
-         * @link https://github.com/yiisoft/db-mysql/commit/a434897691304378edfd787ecee8ad80338aa200
-         */
-        // __DIR__ . '/tests',
-    ]);
-
-    // register a single rule
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-
-    // define sets of rules
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
-    ]);
-
-    $rectorConfig->skip([
+        __DIR__ . '/tests',
+    ])
+    ->withPhpSets(php81: true)
+    ->withRules([
+        InlineConstructorDefaultToPropertyRector::class,
+    ])
+    ->withSkip([
         NullToStrictStringFuncCallArgRector::class,
         ReadOnlyPropertyRector::class,
         RemoveParentCallWithoutParentRector::class,
     ]);
-};
