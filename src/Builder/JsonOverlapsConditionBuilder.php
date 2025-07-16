c862f4821ec4848d6cc3eb3224b90bfffa65726d
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-07-16 09:49:31 +0300

    Improve types

--- src/Builder/JsonOverlapsConditionBuilder.php
+++ src/Builder/JsonOverlapsConditionBuilder.php
@@ -15,6 +15,8 @@ use Yiisoft\Db\QueryBuilder\Condition\JsonOverlapsCondition;
 
 /**
  * Builds expressions for {@see JsonOverlapsCondition} for MySQL Server.
+ *
+ * @extends AbstractOverlapsConditionBuilder<JsonOverlapsCondition>
  */
 final class JsonOverlapsConditionBuilder extends AbstractOverlapsConditionBuilder
 {
@@ -22,7 +24,6 @@ final class JsonOverlapsConditionBuilder extends AbstractOverlapsConditionBuilde
      * Build SQL for {@see JsonOverlapsCondition}.
      *
      * @param JsonOverlapsCondition $expression The {@see JsonOverlapsCondition} to be built.
-     * @psalm-suppress MoreSpecificImplementedParamType
      *
      * @throws Exception
      * @throws InvalidArgumentException
