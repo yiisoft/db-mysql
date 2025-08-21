320fe4efe83c8e732abe3b523731019261e77054
Sergei Tigrov <rrr-r@ya.ru>
2025-08-21 16:31:52 +0700

    Implement `ArrayMergeBuilder`, `LongestBuilder` and `ShortestBuilder` (#420)

---
+++ src/Builder/ArrayMergeBuilder.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Db\Mysql\Builder;
+
+use Yiisoft\Db\Expression\Function\ArrayMerge;
+use Yiisoft\Db\Expression\Function\Builder\MultiOperandFunctionBuilder;
+use Yiisoft\Db\Expression\Function\MultiOperandFunction;
+use Yiisoft\Db\Schema\Column\AbstractArrayColumn;
+use Yiisoft\Db\Schema\Column\ColumnInterface;
+
+use function implode;
+use function is_string;
+use function rtrim;
+
+/**
+ * Builds SQL expressions which merge arrays for {@see ArrayMerge} objects.
+ *
+ * ```sql
+ * (SELECT JSON_ARRAYAGG(value) AS value FROM (
+ *     SELECT value FROM JSON_TABLE(operand1, '$[*]' COLUMNS(value json PATH '$')) AS t
+ *     UNION
+ *     SELECT value FROM JSON_TABLE(operand2, '$[*]' COLUMNS(value json PATH '$')) AS t
+ * ) t)
+ * ```
+ *
+ * @extends MultiOperandFunctionBuilder<ArrayMerge>
+ */
+final class ArrayMergeBuilder extends MultiOperandFunctionBuilder
+{
+    private const DEFAULT_OPERAND_TYPE = 'json';
+
+    /**
+     * Builds a SQL expression which merges arrays from the given {@see ArrayMerge} object.
+     *
+     * @param ArrayMerge $expression The expression to build.
+     * @param array $params The parameters to bind.
+     *
+     * @return string The SQL expression.
+     */
+    protected function buildFromExpression(MultiOperandFunction $expression, array &$params): string
+    {
+        $operandType = $this->buildOperandType($expression->getType());
+        $selects = [];
+
+        foreach ($expression->getOperands() as $operand) {
+            $builtOperand = $this->buildOperand($operand, $params);
+
+            $selects[] = "SELECT value FROM JSON_TABLE($builtOperand, '$[*]' COLUMNS(value $operandType PATH '$')) AS t";
+        }
+
+        return '(SELECT JSON_ARRAYAGG(value) AS value FROM (' . implode(' UNION ', $selects) . ') AS t)';
+    }
+
+    private function buildOperandType(string|ColumnInterface $type): string
+    {
+        if (is_string($type)) {
+            return $type === '' ? self::DEFAULT_OPERAND_TYPE : rtrim($type, '[]');
+        }
+
+        if ($type instanceof AbstractArrayColumn) {
+            if ($type->getDimension() > 1) {
+                return self::DEFAULT_OPERAND_TYPE;
+            }
+
+            $type = $type->getColumn();
+
+            if ($type === null) {
+                return self::DEFAULT_OPERAND_TYPE;
+            }
+        }
+
+        return $this->queryBuilder->getColumnDefinitionBuilder()->buildType($type);
+    }
+}
---
+++ src/Builder/LongestBuilder.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Db\Mysql\Builder;
+
+use Yiisoft\Db\Expression\Function\Builder\MultiOperandFunctionBuilder;
+use Yiisoft\Db\Expression\Function\Longest;
+use Yiisoft\Db\Expression\Function\MultiOperandFunction;
+
+/**
+ * Builds SQL representation of function expressions which returns the longest string from a set of operands.
+ *
+ * ```SQL
+ * (SELECT operand1 AS value
+ * UNION SELECT operand2 AS value
+ * ORDER BY LENGTH(value) DESC LIMIT 1)
+ * ```
+ *
+ * @extends MultiOperandFunctionBuilder<Longest>
+ */
+final class LongestBuilder extends MultiOperandFunctionBuilder
+{
+    /**
+     * Builds a SQL expression to represent the function which returns the longest string.
+     *
+     * @param Longest $expression The expression to build.
+     * @param array $params The parameters to bind.
+     *
+     * @return string The SQL expression.
+     */
+    protected function buildFromExpression(MultiOperandFunction $expression, array &$params): string
+    {
+        $selects = [];
+
+        foreach ($expression->getOperands() as $operand) {
+            $selects[] = 'SELECT ' . $this->buildOperand($operand, $params) . ' AS value';
+        }
+
+        $unions = implode(' UNION ', $selects);
+
+        return "($unions ORDER BY LENGTH(value) DESC LIMIT 1)";
+    }
+}
