eceac62ccd152ddc4ac30e0ab0a5b04c7b205abe
Sergei Tigrov <rrr-r@ya.ru>
2025-09-01 09:07:20 +0700

    Rearrange expression namespaces (#428)

--- src/Builder/JsonOverlapsBuilder.php
+++ src/Builder/JsonOverlapsBuilder.php
@@ -8,9 +8,9 @@ use Yiisoft\Db\Exception\Exception;
 use InvalidArgumentException;
 use Yiisoft\Db\Exception\InvalidConfigException;
 use Yiisoft\Db\Exception\NotSupportedException;
-use Yiisoft\Db\Expression\Builder\ExpressionBuilderInterface;
+use Yiisoft\Db\Expression\ExpressionBuilderInterface;
 use Yiisoft\Db\Expression\ExpressionInterface;
-use Yiisoft\Db\Expression\JsonExpression;
+use Yiisoft\Db\Expression\Value\JsonExpression;
 use Yiisoft\Db\QueryBuilder\Condition\JsonOverlaps;
 use Yiisoft\Db\QueryBuilder\QueryBuilderInterface;
 
