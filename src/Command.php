2568f5226789e84589155caf1ad8a48029b96745
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-10-26 17:36:22 +0300

    improve

--- src/Command.php
+++ src/Command.php
@@ -12,6 +12,8 @@ use Yiisoft\Db\Exception\NotSupportedException;
 use Yiisoft\Db\Query\QueryInterface;
 
 use function in_array;
+use function restore_error_handler;
+use function set_error_handler;
 use function str_starts_with;
 use function substr;
 
