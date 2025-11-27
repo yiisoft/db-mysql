02a9aeb4accffe44953e4b42f7b3f665fe35969f
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-11-27 09:17:34 +0300

    improve

--- tests/ColumnDefinitionParserTest.php
+++ tests/ColumnDefinitionParserTest.php
@@ -7,6 +7,9 @@ namespace Yiisoft\Db\Mysql\Tests;
 use Yiisoft\Db\Mysql\Column\ColumnDefinitionParser;
 use Yiisoft\Db\Tests\Common\CommonColumnDefinitionParserTest;
 
+/**
+ * @group mysql
+ */
 final class ColumnDefinitionParserTest extends CommonColumnDefinitionParserTest
 {
     protected function createColumnDefinitionParser(): ColumnDefinitionParser
